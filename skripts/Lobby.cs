6a516d17e4fc2d3a2144e3976cc8b4ccab0a6548
1mpulseee <lopd.2004@gmail.com>
2022-03-28 22:24:02 +0300

    upd lobby

--- skripts/Lobby.cs
+++ skripts/Lobby.cs
@@ -2,52 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine.UI;
 
 public class Lobby : MonoBehaviourPunCallbacks
 {
     [System.Serializable] public enum DropDown { MainScene, test }
-    [SerializeField] List<GameObject> Buttons = new List<GameObject>();
-
     [SerializeField] public DropDown scene;
+    [SerializeField] List<GameObject> Buttons = new List<GameObject>();
+    private List<RoomInfo> roomList = new List<RoomInfo>();
     private void Awake()
     {
         for (int i = 0; i < Buttons.Count; i++) { Buttons[i].SetActive(false); }
-
-
     }
     void Start()
     {
-        
-        //Debug.Log(PhotonNetwork.MAX_VIEW_IDS);
         PhotonNetwork.NickName = "Player" + Random.Range(1000, 9999); //�������
-        //Log("Player name: " + PhotonNetwork.NickName);
-        //Log("Loading server...");
-        //Log("Please wait");
         PhotonNetwork.AutomaticallySyncScene = true; //���������������� �����
         PhotonNetwork.GameVersion = "1"; //������ ����
         PhotonNetwork.ConnectUsingSettings();
     }
-    public void JoinRoom()
+    public void StartGame()
     {
-        PhotonNetwork.JoinRandomRoom();
+        if (CheckRooms())
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            string RoomName = "room" + Random.Range(0, 10000);
+            PhotonNetwork.CreateRoom(RoomName, new Photon.Realtime.RoomOptions { IsVisible = true, MaxPlayers = 12 }, Photon.Realtime.TypedLobby.Default);
+        }
     }
-    public void CreateRoom()
+    public bool CheckRooms()
     {
-        string RoomName = "room" + Random.Range(0, 10000);
-        PhotonNetwork.CreateRoom(RoomName, new Photon.Realtime.RoomOptions { IsVisible = true, MaxPlayers = 12 }, Photon.Realtime.TypedLobby.Default);
+        if (roomList.Count > 0)
+        {
+            for (int i = 0; i < roomList.Count; i++)
+            {
+                if (roomList[i].PlayerCount < roomList[i].MaxPlayers)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        else
+        {
+            return false;
+        }
     }
+    #region Callbacks
     public override void OnConnectedToMaster()
     {
         Debug.Log("Server loaded");
-        for (int i = 0; i < Buttons.Count; i++) {Buttons[i].SetActive(true);}
-
-
-
+        for (int i = 0; i < Buttons.Count; i++) { Buttons[i].SetActive(true); }
         PhotonNetwork.JoinLobby();
     }
     public override void OnJoinedRoom()
     {
         PhotonNetwork.LoadLevel(scene.ToString());
     }
+    public override void OnRoomListUpdate(List<RoomInfo> roomListUpd)
+    {
+        roomList = roomListUpd;
+    }
+    #endregion
 }
