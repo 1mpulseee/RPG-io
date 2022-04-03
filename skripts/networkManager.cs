d0f1e66a04d7786128f8bd50e0387a8f7d491384
1mpulseee <lopd.2004@gmail.com>
2022-04-03 17:01:39 +0300

    upd

--- skripts/networkManager.cs
+++ skripts/networkManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 
-public class networkManager : MonoBehaviourPunCallbacks
+public class networkManager : MonoBehaviour//PunCallbacks
 {
     public TerrainGenerator terrainGenerator;
     private PhotonView PV;
